ec6f7240d0e16d98b6680686427d9e506e0e249c
Ahsanuzzaman Khan <ahsan@Ahsanuzzamans-MacBook-Air.local>
2022-03-05 20:01:38 +0600

    Add heroku config files.

---
+++ Procfile
@@ -0,0 +1,3 @@
+release: python manage.py migrate
+
+web: gunicorn file_protector.wsgi --log-file -
--- file_protector/settings.py
+++ file_protector/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--z!=hrj&abmtt!l2-v%n@6!1j8^fge*l@%3273-l&-n7#^z7*i'
+SECRET_KEY = config("SECRET_KEY", default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False)
